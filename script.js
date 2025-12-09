671ace6f26ebfedfd96fe405c1c737a2b803d8c9
sofiaas16 <sarahzambrano2019@gmail.com>
2025-12-09 10:46:52 -0500

    feat: :sparkles: MockAPI
    I changed the structure of the JS bc I needed to use mockAPI

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const API_URL = "https://69383e584618a71d77cf8252.mockapi.io/todo/api/task";
 
-function addTask() {
+
+async function fetchTasks() {
+    const response = await fetch(API_URL);
+    const tasks = await response.json();
+    listContainer.innerHTML = "";
+    tasks.forEach(task => {
+        addTaskToDOM(task);
+    });
+}
+
+
+function addTaskToDOM(task) {
+    let li = document.createElement("li");
+    li.textContent = task.name;
+    li.dataset.id = task.id;
+
+    if(task.state) li.classList.add("checked");
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    listContainer.appendChild(li);
+}
+
+
+async function addTask() {
     if(inputBox.value === '') {
         alert("You must write something!");
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);   
-        } 
-        inputBox.value = "";
-        saveData();
+        return;
+    }
+
+    const newTask = {
+        name: inputBox.value,
+        state: false
+    };
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTask)
+    });
+
+    const createdTask = await response.json();
+    addTaskToDOM(createdTask);
+    inputBox.value = "";
 }
 
-listContainer.addEventListener("click", function(e) {
+// Manejo de clics (marcar completada o borrar)
+listContainer.addEventListener("click", async function(e) {
+    const li = e.target.tagName === "LI" ? e.target : e.target.parentElement;
+    const id = li.dataset.id;
+
     if(e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
+        li.classList.toggle("checked");
+        await fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ state: li.classList.contains("checked") })
+        });
+    } else if(e.target.tagName === "SPAN") {
+        await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+        li.remove();
     }
-    else if(e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
+});
 
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-
-showTask();
+fetchTasks();
